e76270503fd37b38b71ecd2dd8be5d812d8c550b
Алексей Якубин <2412719@mail.ru>
2021-09-24 15:22:41 +0300

    оправлена настройка, в.0.0.2

--- src/IdempotencyServiceCollectionExtensions.cs
+++ src/IdempotencyServiceCollectionExtensions.cs
@@ -16,24 +16,27 @@ namespace Delobytes.AspNetCore.Idempotency
         /// <param name="services"><see cref="IServiceCollection"/> в которую нужно добавить контроль идемпотентности.</param>
         /// <param name="configure"><see cref="Action{IdempotencyControlOptions}"/> для настройки <see cref="IdempotencyControlOptions"/>.</param>
         /// <returns>Ссылка на этот экземпляр после завершения операции.</returns>
-        public static IServiceCollection AddIdempotencyControl(this IServiceCollection services, Action<IdempotencyControlOptions> configure)
+        public static IServiceCollection AddIdempotencyControl(this IServiceCollection services, Action<IdempotencyControlOptions> configure = null)
         {
             if (services is null)
             {
                 throw new ArgumentNullException(nameof(services));
             }
 
-            if (configure is null)
-            {
-                throw new ArgumentNullException(nameof(configure));
-            }
+            ////if (configure is null)
+            ////{
+            ////    throw new ArgumentNullException(nameof(configure));
+            ////}
 
             if (!services.Any(x => x.ServiceType == typeof(IDistributedCache)))
             {
                 throw new InvalidOperationException("An IDistributedCache provider must be registered for idempotency control.");
             }
 
-            services.Configure(configure);
+            if (configure is not null)
+            {
+                services.Configure(configure);
+            }
 
             services.AddScoped<IdempotencyFilterAttribute>();
 
